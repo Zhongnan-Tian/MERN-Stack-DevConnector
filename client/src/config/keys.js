ad17f41b1feffe006f4ab94f0b79f8e91ea4702a
unknown <znancytian@gmail.com>
2019-03-22 12:16:01 -0400

    Modify client config file

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,12 +1,24 @@
+/* For Development */
+// import CLIENT_SECRET_PROD from './keys_prod';
+// import CLIENT_SECRET_DEV from './keys_dev';
+
+// let CLIENT_SECRET;
+
+// if (process.env.NODE_ENV === 'production') {
+//   CLIENT_SECRET = CLIENT_SECRET_PROD;
+// } else {
+//   CLIENT_SECRET = CLIENT_SECRET_DEV;
+// }
+
+// export default CLIENT_SECRET;
+
+/* For Production */
 import CLIENT_SECRET_PROD from './keys_prod';
-import CLIENT_SECRET_DEV from './keys_dev';
 
 let CLIENT_SECRET;
 
 if (process.env.NODE_ENV === 'production') {
   CLIENT_SECRET = CLIENT_SECRET_PROD;
-} else {
-  CLIENT_SECRET = CLIENT_SECRET_DEV;
 }
 
 export default CLIENT_SECRET;
